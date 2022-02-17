42d27afaf9b5e199bff688e349267599c64481d0
Xiaohan Wu <jessicawu@Xiaohans-MBP.lan>
2022-02-17 01:28:19 -0600

    Updated the figure

--- R/output/figure_arrests_by_racegender.R
+++ R/output/figure_arrests_by_racegender.R
@@ -3,16 +3,16 @@
 
 read_csv(here("data/NLSY97_clean.csv")) %>%
   group_by(race, gender) %>%
-  summarize(total_arrests = mean(total_arrests)) %>%
-  ggplot(aes(race, total_arrests, fill = gender)) +
+  summarize(total_incarcerated = mean(total_incarcerated)) %>%
+  ggplot(aes(race, total_incarcerated, fill = gender)) +
   geom_bar(stat = "identity", position = "dodge") +
   labs(
     x = "Race", 
-    y = "Mean Arrests", 
+    y = "Mean Incarcerated", 
     fill = "Gender",
-    title = "Mean Number of Arrests in 2002 by Race and Gender") +
+    title = "Mean Number of Incarcerated in 2002 by Race and Gender") +
   theme_minimal() +
   scale_fill_economist()
 
-ggsave(here("figures/arrests_by_racegender.png"), width=8, height=4.5)
+ggsave(here("figures/incarcerated_by_racegender.png"), width=8, height=4.5)
 
